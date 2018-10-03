412b3e65b29e79f78c6164b132b47899c6163524
John <john.s.dulaney1@gmail.com>
2018-10-03 17:00:15 -0500

    :fire:

---
+++ components/Body.js
@@ -0,0 +1,5 @@
+export default () => (
+    <div>
+        Helloooooooooooooooo
+    </div>
+  );
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
-
 import Link from "next/link";
+import Link from "../components/Body";
+
 
 export default () => (
   <main>
     <p>Welcome to Next.js!</p>
+    <Body />
     <Link href="/john">
       <a>See John.js</a>
     </Link>
