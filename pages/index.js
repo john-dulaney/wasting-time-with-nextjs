7a66fada6f10de6bfea4b8e53e107f455a4687d8
root <root@TNHQBQPQ2.goprecise.com>
2018-10-03 16:18:19 -0500

    :penguin: Linux in my way

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+
+import Link from "next/link";
+
+export default () => (
+  <main>
+    <p>Welcome to Next.js!</p>
+    <Link href="/john">
+      <a>See John.js</a>
+    </Link>
+    </main>
+  )
